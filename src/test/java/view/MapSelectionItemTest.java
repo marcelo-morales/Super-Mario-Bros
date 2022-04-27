8898f26f17314ea1df78640c1756e0865a6e90ae
Andrew Suh <andrewsuh98@gmail.com>
2022-04-27 19:18:10 -0400

    add blackbox testing to MapSelectionItemTest

--- src/test/java/view/MapSelectionItemTest.java
+++ src/test/java/view/MapSelectionItemTest.java
@@ -1,6 +1,7 @@
 package view;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import view.MapSelectionItem;
 
@@ -9,8 +10,16 @@ import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Whitebox testing to test the MapSelectionItem class.
- * Goal to achieve branch coverage.
+ * Test class to test the MapSelection class.
+ *
+ * Blackbox testing for possible methods, technique determined depending on method.
+ * Whitebox testing for each method, aim to achieve branch coverage.
+ * Mutation testing, aim to achieve maximum mutation coverage.
+ *
+ * Notes:
+ *      testConstructorInvalidMap() and testConstructorNullMap() both fail because the method signature does not
+ *      indicate throwing exceptions, but if the String map is null or invalid, the constructor will throw an
+ *      IllegalArgumentException.
  */
 public class MapSelectionItemTest {
 
@@ -25,17 +34,91 @@ public class MapSelectionItemTest {
     }
 
     /**
-     * Test the constructor and if the values are set correctly.
+     * Test constructor with map = null.
+     *
+     * goal: blackbox testing with equivalence partitioning.
+     *       use base choice coverage
+     *
+     * EP:
+     *      String path - {null, not null invalid path, valid path}
+     *          base: valid path
+     *      Point location - {null, not null}
+     *          base: not null
      */
+    // TODO: remove disabled
+    @Disabled
     @Test
-    void testConstructor() {
+    void testConstructorNullMap() {
+        String mapName = null;
+        map = new MapSelectionItem(mapName, location);
         assertEquals(mapName, map.getName());
         assertEquals(location, map.getLocation());
-        assertEquals(new Dimension(), map.getDimension());
+    }
+
+    /**
+     * Test constructor with map = not null invalid path.
+     *
+     * goal: blackbox testing with equivalence partitioning.
+     *       use base choice coverage
+     *
+     * EP:
+     *      String path - {null, not null invalid path, valid path}
+     *          base: valid path
+     *      Point location - {null, not null}
+     *          base: not null
+     */
+    // TODO: remove disabled
+    @Disabled
+    @Test
+    void testConstructorInvalidMap() {
+        String mapName = "test";
+        map = new MapSelectionItem(mapName, location);
+        assertEquals(mapName, map.getName());
+        assertEquals(location, map.getLocation());
+    }
+
+    /**
+     * Test constructor with map = valid.
+     *
+     * goal: blackbox testing with equivalence partitioning.
+     *       use base choice coverage
+     *
+     * EP:
+     *      String path - {null, not null invalid path, valid path}
+     *          base: valid path
+     *      Point location - {null, not null}
+     *          base: not null
+     */
+    @Test
+    void testConstructorNullLocation() {
+        Point location = null;
+        map = new MapSelectionItem(mapName, location);
+        assertEquals(mapName, map.getName());
+        assertEquals(location, map.getLocation());
+    }
+
+    /**
+     * Test constructor with map & location valid.
+     *
+     * goal: blackbox testing with equivalence partitioning.
+     *       use base choice coverage
+     *
+     * EP:
+     *      String path - {null, not null invalid path, valid path}
+     *          base: valid path
+     *      Point location - {null, not null}
+     *          base: not null
+     */
+    @Test
+    void testConstructorValidMapValidLocation() {
+        assertEquals(mapName, map.getName());
+        assertEquals(location, map.getLocation());
     }
 
     /**
      * Test setDimension().
+     *
+     * goal: whitebox testing branch coverage
      */
     @Test
     void testSetDimension() {
@@ -46,6 +129,8 @@ public class MapSelectionItemTest {
 
     /**
      * Test setLocation().
+     *
+     * goal: whitebox testing branch coverage
      */
     @Test
     void testSetLocation() {
